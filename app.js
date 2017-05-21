0626ee14bdd5b69260848ea72af2b5f35c845651
acheerio <acheerioo@gmail.com>
2017-05-20 23:41:03 -0500

    load initial user and group info
    - modified to use express request module
    - nested GET calls to retrieve user data
    - display group information
    - display images

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
 
+var IMG_SIZE = "avatar"; // options (small to large): avatar, preview, large
+
 app.get('/', function(req, res){
 	res.render("home");
 });
@@ -32,15 +34,70 @@ app.get('/about', function(req, res){
 });
 
 app.get('/2', function(req, res){
+	
+	// get the user's API key from URL
 	req.session.apiKey = req.query.access_token || "";	
 	
+	// if the user is not connected, prompt to connect to GroupMe
 	if (req.session.apiKey == "")
 	{
 		res.render("2_login");
 	}
+	// if the user IS connected, fetch data with valid API key
 	else
 	{
-		res.render("2_demo");
+		var context = {};
+		
+		// request user data
+		request.get({url: "https://api.groupme.com/v3/users/me?token=" + req.session.apiKey}, function(error, response, body)
+		{
+			if (!error && response.statusCode >= 200 && response.statusCode < 400)
+			{
+				var userObj = JSON.parse(body).response;
+				// save to session
+				req.session.img = userObj.image_url; // to show avatar
+				req.session.name = userObj.name; // to show personalized greeting
+				// save to context
+				context.img = req.session.img + "." + IMG_SIZE;
+				context.name = req.session.name;
+				context.id = userObj.id;
+				context.email = userObj.email;
+				context.phone = userObj.phone_number;
+				context.lang = userObj.locale;
+				
+				// request group data
+				request.get({url: "https://api.groupme.com/v3/groups?token=" + req.session.apiKey}, function(error, response, body)
+				{
+					if (!error && response.statusCode >= 200 && response.statusCode < 400)
+					{
+						var myGroups = [];
+						var groups = JSON.parse(body).response;
+						for (var i = 0; i < groups.length; i++)
+						{
+							myGroups.push({
+								"img": groups[i]["image_url"] + "." + IMG_SIZE,
+								"name": groups[i]["name"],
+								"id": groups[i]["id"],
+								"privacy": groups[i]["type"],
+								"num_members": groups[i]["members"].length
+							});
+						}
+						context.groups = myGroups;
+						res.render("2_demo", context);
+					}
+					else
+					{
+						// error
+						res.status(response.statusCode);
+					}
+				});
+			}
+			else
+			{
+				// action if error
+				res.status(response.statusCode);
+			}
+		});
 	}
 });
 
@@ -53,7 +110,14 @@ app.get('/headers', function(req,res){
 });
 */
 
+/*
 app.get('/api', function(req, res){
+	if (req.body.command === "disconnect")
+	{
+		req.session.destroy();
+	}
+	// need "else error"?
+	
 	if (req.query.q == "user")
 	{	
 		var userObj = null;
@@ -62,13 +126,14 @@ app.get('/api', function(req, res){
 				if (!error && response.statusCode >= 200 && response.statusCode < 400)
 				{
 					userObj = JSON.parse(body).response;
+					console.log(userObj); // testing
 				}
 				res.json(userObj);
 				res.status(response.statusCode);
 			}
 		);
 	}
-});
+});*/
 
 // custom 404 page
 app.use(function(req, res){
